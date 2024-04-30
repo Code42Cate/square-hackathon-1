d9bf1acc20b060520c1f8751c29f8fa03491ad3e
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-04-30 21:24:35 +0200

    feat: webhooks handler

--- apps/web/app/api/webhooks/route.ts
+++ apps/web/app/api/webhooks/route.ts
@@ -1,3 +1,6 @@
+import { prisma } from "database";
+import { Client, Environment } from "square";
+
 type OrderCreatedEvent = {
   type: "order_created";
   id: string;
@@ -12,10 +15,66 @@ type OrderCreatedEvent = {
   };
 };
 
+// SQUARE webhook handler that listens for order_created events
 export async function POST(request: Request) {
-  console.log("Received webhook request", request);
+  const body: OrderCreatedEvent = await request.json();
 
-  console.log("Request body", await request.text());
+  const client = new Client({
+    environment: Environment.Sandbox,
+    accessToken: getUserAccessToken(),
+  });
+
+  // retrieve order
+  const { result } = await client.ordersApi.retrieveOrder(body.id);
+
+  if (result.errors) {
+    console.error("Error retrieving order", result.errors);
+    return new Response("Error", { status: 500 });
+  }
+
+  // get all purchased items
+  const itemsIds = result.order.lineItems.map((item) => item.uid);
+
+  // only retrieve items that shop owners have agreed to donate a percentage of
+  const items = await prisma.product.findMany({
+    where: {
+      squareItemId: {
+        in: itemsIds,
+      },
+    },
+  });
+
+  // calculate total amount contributed
+  const totalContributed = items.reduce(
+    (total, item) => total + item.price * (item.percentageContributed / 100),
+    0,
+  );
+
+  // find the current project
+  const project = await prisma.project.findFirst({
+    orderBy: {
+      likeCount: "desc",
+    },
+  });
+
+  if (!project) {
+    console.error("No projects found");
+    return new Response("Error", { status: 500 });
+  }
+
+  // update the project with the new funding amount
+  await prisma.project.update({
+    where: {
+      id: project.id,
+    },
+    data: {
+      fundingCurrent: (project.fundingCurrent += totalContributed),
+    },
+  });
 
   return new Response("OK", { status: 200 });
+}
+
+function getUserAccessToken(userId?: string) {
+  return "EAAAl3uEAmzADix5JVWyLWX3o5_j3i95hjFtmwVUQN4dhH1jhKHhfS5lqrD6nx74";
 }
