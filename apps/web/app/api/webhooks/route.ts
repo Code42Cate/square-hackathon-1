5a746612b19074f588b3f7e9effc3a129513ca43
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-04-30 21:09:13 +0200

    fix: method

--- apps/web/app/api/webhooks/route.ts
+++ apps/web/app/api/webhooks/route.ts
@@ -1,4 +1,18 @@
-export async function GET(request: Request) {
+type OrderCreatedEvent = {
+  type: "order_created";
+  id: string;
+  object: {
+    order_created: {
+      created_at: string;
+      location_id: string;
+      order_id: string;
+      state: string;
+      version: 1;
+    };
+  };
+};
+
+export async function POST(request: Request) {
   console.log("Received webhook request", request);
 
   console.log("Request body", await request.text());
